19b3e6a8e5cb572006a65b5b30aea879757b1591
Vaibhav <vrongmeal@gmail.com>
2020-02-26 20:11:51 +0530

    Initial commit
    Signed-off-by: Vaibhav <vrongmeal@gmail.com>

---
+++ README.md
@@ -0,0 +1,19 @@
+# homebrew-tap
+
+> My tap for Homebrew.
+
+## Contents
+
+1. [Usage](#usage)
+1. [Available Formulae](#available-formulae)
+
+## Usage
+
+```console
+> brew tap vrongmeal/tap
+> brew install vrongmeal/tap/<formula>
+```
+
+## Available Formulae
+
+* [Leaf](https://github.com/vrongmeal/leaf)
---
+++ formulae/leaf.rb
@@ -0,0 +1,13 @@
+class Leaf < Formula
+  desc "General purpose hot-reloader for all projects."
+  homepage "https://github.com/vrongmeal/leaf"
+  url "https://github.com/vrongmeal/leaf/archive/v1.1.0.tar.gz"
+  sha256 "b96bef79ca888fa5f44213a47af6d7069198539f16705094cbddda755aed5e28"
+
+  depends_on "go" => :build
+
+  def install
+    system "make", "build"
+    bin.install Dir["build/*"]
+  end
+end
