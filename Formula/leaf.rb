a41759eb0604365f636982bd4a3b42c7cade13e6
Vaibhav <vrongmeal@gmail.com>
2020-04-21 12:32:05 +0530

    Correct version of leaf to `v1.2.0`.

--- Formula/leaf.rb
+++ Formula/leaf.rb
@@ -1,7 +1,7 @@
 class Leaf < Formula
   desc "General purpose hot-reloader for all projects."
   homepage "https://github.com/vrongmeal/leaf"
-  url "https://github.com/vrongmeal/leaf/archive/v2.0.0.tar.gz"
+  url "https://github.com/vrongmeal/leaf/archive/v1.2.0.tar.gz"
   sha256 "b96bef79ca888fa5f44213a47af6d7069198539f16705094cbddda755aed5e28"
 
   depends_on "go" => :build
